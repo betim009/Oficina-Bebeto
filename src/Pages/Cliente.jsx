49a2511f113a4879157c05e9439920eb85802bcf
Alberto <betim009@gmail.com>
2023-04-18 12:33:54 -0300

    feat: pequenas mudanças na tentativa de corrigir warnings causado pelo qrReader

--- src/Pages/Cliente.jsx
+++ src/Pages/Cliente.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState, useCallback } from "react";
 import { QrReader } from 'react-qr-reader';
 import '../App.css';
 import Context from '../Context/Context';
@@ -7,23 +7,24 @@ import Footer from "../Components/Footer";
 
 function Cliente() {
   const navigate = useNavigate();
-  const { 
+  const {
     selectedClientId,
-    setSelectedClientId, 
-    selectedClient, 
+    setSelectedClientId,
+    selectedClient,
     setSelectedClientById
-    } = useContext(Context);
+  } = useContext(Context);
+
+  const [qrCodeResult, setQrCodeResult] = useState(null);
 
   const handleScan = (data) => {
     if (data) {
-      setSelectedClientId(parseInt(data));
+      setQrCodeResult(data);
     }
-    console.log(data)
-  }
+  };
 
   const handleError = (err) => {
     console.error(err);
-  }
+  };
 
   useEffect(() => {
     if (selectedClientId !== null) {
@@ -35,20 +36,32 @@ function Cliente() {
     setSelectedClientId(parseInt(e.target.value));
   }
 
+  const handleQrCodeResult = useCallback(() => {
+    if (qrCodeResult !== null) {
+      setSelectedClientId(parseInt(qrCodeResult));
+      setQrCodeResult(null);
+    }
+  }, [qrCodeResult, setSelectedClientId]);
+
+  useEffect(() => {
+    handleQrCodeResult();
+  }, [handleQrCodeResult]);
+
   return (
     <div className="d-flex justify-content-center align-items-center h-100">
       <div className="text-center">
         <h2 className="mt-4 mb-4">Identificação do Cliente</h2>
-        <h5 className="display-5" style={{fontSize: "20px"}}>POR QRcode</h5> 
+        <h5 className="display-5" style={{ fontSize: "20px" }}>POR QRcode</h5>
         <div className="Scan mb-4">
           <QrReader
             delay={300}
             onError={handleError}
             onScan={handleScan}
+            onResult={(result) => console.log(result)}
           />
         </div>
         <div>
-          <h4 className="display-5" style={{fontSize: "20px"}}>Por ID:</h4>
+          <h4 className="display-5" style={{ fontSize: "20px" }}>Por ID:</h4>
           <input type="text" onChange={handleInputChange} />
           {selectedClient !== undefined ?
             <div className="mt-4">
